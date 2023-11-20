79d754e128929b04ec4d7a35d1070d29487d5ce9
YaroslavKyba <narxoc.ne@gmail.com>
2023-11-20 23:34:55 +0200

    second commit

--- src/js/API.js
+++ src/js/API.js
@@ -1,24 +1,47 @@
 import axiosCl from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const axios = axiosCl.create({
-  baseURL: 'https://pixabay.com/api/',
-  params: {
-    key: '40789881-740a9124b121bb9def1cb2b55',
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-  },
-});
+export default class ImgAPIService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
 
-export function fetchImage(q) {
-  return axios.get('', {
-    params: { q },
-  });
-}
+    this.axios = axiosCl.create({
+      baseURL: 'https://pixabay.com/api/',
+      params: {
+        key: '40789881-740a9124b121bb9def1cb2b55',
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: this.page,
+        per_page: 40,
+      },
+    });
+  }
 
-export function errorNotification() {
-  Notify.failure(
-    'Sorry, there are no images matching your search query. Please try again.'
-  );
+  fetchImage() {
+    return this.axios
+      .get('', {
+        params: { q: this.searchQuery },
+      })
+      .then(imgData => {
+        this.page += 1;
+        // createMarkup(imgData.data.hits, gallery);
+      })
+      .catch(_ => newApiService.errorNotification());
+  }
+
+  errorNotification() {
+    Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
 }
--- src/js/index..js
+++ src/js/index..js
@@ -1,9 +1,11 @@
-import { fetchImage, errorNotification } from './API';
+import ImgAPIService from './API';
 import { createMarkup } from './markup';
 
 const searchQuery = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
+
+const newApiService = new ImgAPIService();
 
 searchQuery.addEventListener('submit', onSubmit);
 loadMoreBtn.addEventListener('click', onLoadMore);
@@ -11,15 +13,21 @@ loadMoreBtn.addEventListener('click', onLoadMore);
 function onSubmit(ev) {
   ev.preventDefault();
 
-  const searchValue = ev.currentTarget.searchQuery.value;
+  newApiService.query = ev.currentTarget.searchQuery.value;
 
-  fetchImage(searchValue)
-    .then(imgData => {
-      createMarkup(imgData.data.hits, gallery);
-    })
-    .catch(_ => errorNotification());
+  newApiService.fetchImage();
+  // .then(imgData => {
+  //   createMarkup(imgData.data.hits, gallery);
+  // })
+  // .catch(_ => newApiService.errorNotification());
 
   //   ev.currentTarget.reset();
 }
 
-function onLoadMore() {}
+function onLoadMore() {
+  newApiService.fetchImage();
+  // .then(imgData => {
+  //   createMarkup(imgData.data.hits, gallery);
+  // })
+  // .catch(_ => errorNotification());
+}
